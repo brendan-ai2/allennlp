9914b9071cf4917944d9137df108e9ba72b9dbef
Brendan Roof <brendanr@allenai.org>
2018-07-14 00:02:10 +0000

    Add test template.

--- allennlp/training/metrics/sequence_accuracy.py
+++ allennlp/training/metrics/sequence_accuracy.py
@@ -26,10 +26,9 @@ class SequenceAccuracy(Metric):
         Parameters
         ----------
         predictions : ``torch.Tensor``, required.
-            A tensor of predictions of shape (batch_size, ..., num_classes).
+            A tensor of predictions of shape (batch_size, top_k, sequence_length).
         gold_labels : ``torch.Tensor``, required.
-            A tensor of integer class label of shape (batch_size, ...). It must be the same
-            shape as the ``predictions`` tensor without the ``num_classes`` dimension.
+            A tensor of integer class label of shape (batch_size, sequence_length).
         mask: ``torch.Tensor``, optional (default = None).
             A masking tensor the same size as ``gold_labels``.
         """
@@ -50,9 +49,6 @@ class SequenceAccuracy(Metric):
         if gold_labels.dim() != predictions.dim() - 1:
             raise ConfigurationError("gold_labels must have dimension == predictions.size() - 1 but "
                                      "found tensor of shape: {}".format(predictions.size()))
-        if (gold_labels >= num_classes).any():
-            raise ConfigurationError("A gold label passed to Categorical Accuracy contains an id >= {}, "
-                                     "the number of classes.".format(num_classes))
 
         # Top K indexes of the predictions (or fewer, if there aren't K of them).
         # Special case topk == 1, because it's common and .max() is much faster than .topk().
