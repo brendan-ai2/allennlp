a876fb68b5b39084f397f5f63d7d6305b9f16242
Brendan Roof <brendanr@allenai.org>
2018-11-26 23:01:53 -0800

    wip

--- allennlp/data/iterators/multiprocess_iterator.py
+++ allennlp/data/iterators/multiprocess_iterator.py
@@ -28,7 +28,10 @@ def _create_tensor_dicts(input_queue: Queue,
             yield instance
             instance = input_queue.get()
 
+    count = 0
     for tensor_dict in iterator(instances(), num_epochs=1, shuffle=shuffle):
+        if count % 10 == 0: # REMOVEME
+            print(f"approximate output_queue size: {output_queue.qsize()}") # REMOVEME
         output_queue.put(tensor_dict)
 
     output_queue.put(index)
